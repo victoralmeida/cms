84107ea203ae8f716423f995cb0fe241002a8b4c
Victor Almeida <victordominguesalmeida@gmail.com>
2018-01-29 22:42:14 -0200

    Subindo pasta raiz

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php   // REQUIRES SCRIPTS
+	
+	echo '<style>';
+		require('css/script.css');
+	echo '</style>';
+
+
+	echo '<script>';
+		require('js/script.js');
+	echo '</script>';
+
+?>
+
+<?php  // REQUIRES VIEWS
+
+	require('views/menu.html');
+	require('views/content1.html');
+	require('views/content2.html');
+
+?>
---
+++ js/script.js
@@ -0,0 +1,10 @@
+function show(id){
+        var botao = document.getElementById('botao');
+       	var contentId = document.getElementById(id);
+       	var contentClass = getElementsByClassName('content');
+
+       	//contentClass.style.display = 'none';
+       	contentId.style.display = 'block';
+
+
+}
